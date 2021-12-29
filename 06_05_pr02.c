0bce962fea2476e818d35239b83795b8be9e2e1a
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2021-12-29 16:37:57 +0530

    Pointer

--- 06_05_pr02.c
+++ 06_05_pr02.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-
-void printAdd(int a){
-    printf("The address of variable a is %u\n", &a);
-}
-
-int main(){
-    int i = 4;
-    printf("The value of variable i is %d\n", i);
-    printAdd(i);
-    printf("The address of variable i is %u\n", &i);
-    return 0;
-}
+#include<conio.h>
+void printAdd(int a)
+    {
+        printf("The address of variable a is %u\n",&a);
+    }
+int main()
+    {
+        int i=4;
+        printf("The value of variable i is %d\n",i);
+        printAdd(i);
+        printf("The address of variable i is %u\n",&i);
+        return 0;
+    }
