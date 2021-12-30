69d05788d4b9417f4f4606a90283d2d86da9714d
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2021-12-30 10:00:16 +0530

    Array

--- 07_05_pointer_arithmetic.c
+++ 07_05_pointer_arithmetic.c
@@ -1,35 +1,11 @@
 #include<stdio.h>
-
-int main(){
-    // int i = 34;
-    // int *ptr = &i;
-    // printf("The value of ptr is %u\n", ptr);
-    // // ptr = ptr - 1;
-    // ptr = ptr + 1;
-    // // ptr++;
-    // // ptr++;
-    // printf("The value of ptr is %u\n", ptr);
-
-    // char c = '3';
-    // char *ptr = &c;
-    // printf("The value of ptr is %u\n", ptr);
-    // // ptr = ptr - 1;
-    // ptr = ptr + 1;
-    // ptr = ptr + 1;
-    // // ptr++;
-    // // ptr++;
-    // printf("The value of ptr is %u\n", ptr);
-
-    float f = 3.4;
-    float *ptr = &f;
-    printf("The value of ptr is %u\n", ptr);
-    // ptr = ptr - 1;
-    ptr = ptr + 1;
-    // ptr = ptr + 1;
-    // ptr++;
-    // ptr++;
-    printf("The value of ptr is %u\n", ptr);
-
-
-    return 0;
-}
+#include<conio.h>
+int main()
+    {
+        float f=3.4;
+        float *ptr=&f;
+        printf("The value of ptr is %u\n",ptr);
+        ptr=ptr+1;
+        printf("The value of ptr is %u\n",ptr);
+        return 0;
+    }
--- 07_10_pr03.c
+++ 07_10_pr03.c
@@ -1,13 +1,16 @@
+/*Program to print table of a given no.*/
 #include<stdio.h>
-
-int main(){
-    int mul[10];
-    for(int i=0; i<10; i++){
-        mul[i] = 5*(i+1);
+#include<conio.h>
+int main()
+    {
+        int mul[10];
+        for(int i=0;i<10;i++)
+            {
+                mul[i]=5*(i+1);
+            }
+        for(int i=0;i<10;i++)
+            {
+                printf("5X%d=%d\n",i+1,mul[i]);
+            }
+        return 0;
     }
-
-    for(int i=0; i<10; i++){
-        printf("5X%d = %d\n", i+1, mul[i]);
-    }
-    return 0;
-}
