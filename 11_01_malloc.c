1dc44ec3b6f170e3b1d3a808ba6d383c85b6fec5
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2022-01-09 08:52:07 +0530

    Malloc/Calloc

--- 11_01_malloc.c
+++ 11_01_malloc.c
@@ -1,20 +1,18 @@
 #include<stdio.h> 
 #include<stdlib.h> 
-
-int main(){
-    int *ptr;
-    // Sizeof operator in C
-    // printf("The size of int on my pc is %d\n", sizeof(int));
-    // printf("The size of float on my pc is %d\n", sizeof(float));
-    // printf("The size of char on my pc is %d\n", sizeof(char));
-    ptr = (int *) malloc(6 * sizeof(int)); 
-    for(int i=0; i<6;i++){
-        printf("Enter the value of %d element: \n", i); 
-        scanf("%d", &ptr[i]);
+#include<conio.h>
+int main()
+    {
+        int *ptr;
+        ptr = (int *)malloc(6*sizeof(int)); 
+        for(int i=0;i<6;i++)
+            {
+                printf("\nEnter the value of %d element:",i); 
+                scanf("%d",&ptr[i]);
+            }
+        for(int i=0;i<6;i++)
+            {
+                printf("\nThe value of %d element is:%d",i,ptr[i]); 
+            } 
+        return 0;
     }
-
-    for(int i=0; i<6;i++){
-        printf("The value of %d element is: %d\n", i, ptr[i]); 
-    }
-    return 0;
-}
--- 11_02_calloc.c
+++ 11_02_calloc.c
@@ -1,16 +1,13 @@
 #include<stdio.h> 
 #include<stdlib.h> 
-
-int main(){
-    int *ptr; 
-    ptr = (int *) calloc(6, sizeof(int)); 
-    // for(int i=0; i<6;i++){
-    //     printf("Enter the value of %d element: \n", i); 
-    //     scanf("%d", &ptr[i]);
-    // }
-
-    for(int i=0; i<6;i++){
-        printf("The value of %d element is: %d\n", i, ptr[i]); 
+#include<conio.h>
+int main()
+    {
+        int *ptr; 
+        ptr = (int *)calloc(6,sizeof(int));
+        for(int i=0;i<6;i++)
+            {
+                printf("\nThe value of %d element is:%d",i,ptr[i]); 
+            }
+        return 0;
     }
-    return 0;
-}
