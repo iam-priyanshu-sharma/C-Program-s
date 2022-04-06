a3670d2554fc735d79452a739c267518fbd584c8
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2022-04-06 17:30:35 +0530

    add .gitignore file

--- 2NDLAR02.C
+++ 2NDLAR02.C
@@ -5,7 +5,6 @@ int main()
    {
      double n1,n2,n3;
      printf("Enter three different numbers:");
-     scanf("%lf%lf%lf",&n1,&n2,&n3);
      if(n1>=n2 && n1>=n3)
 	printf("%.2lf is the largest number.",n1);
      if(n2>=n1 && n2>=n3)
